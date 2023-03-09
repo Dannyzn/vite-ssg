9b5289fb19ddb164c59a6bee9f965ba7ad9afca8
Dannyzn <694518723@qq.com>
2023-03-09 23:35:09 +0800

    feat: 增加初始化渲染的方法

---
+++ src/layout/runtime/client-entry.tsx
@@ -0,0 +1,14 @@
+import React = require("react");
+import { createRoot } from "react-dom/client";
+import { App } from "../runtime/App";
+
+function renderInBrowser() {
+    // This is the entry point for the browser
+    const root = document.getElementById("root");
+    if (!root) {
+        throw new Error("Could not find root element");
+    }
+  createRoot(root).render(<App />);
+}
+
+renderInBrowser();
