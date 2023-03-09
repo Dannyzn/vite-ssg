741321e5b31d222ec0178c5aa9a234e4f1cec76b
Dannyzn <694518723@qq.com>
2023-03-09 22:57:12 +0800

    no-commit

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -2,4 +2,4 @@ import { join } from "path";
 
 export const PACKAGE_ROOT = join(__dirname, "..", "..", "..");
 
-export const CLIENT_ENTRY_PATH = join(PACKAGE_ROOT, "template.html");
+export const DEFAULT_HTML_PATH = join(PACKAGE_ROOT, "template.html");
