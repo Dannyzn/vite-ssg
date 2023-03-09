aa9809c7aeadc299f44730bbfb04ff5faa450ef4
Dannyzn <694518723@qq.com>
2023-03-09 22:54:26 +0800

    给vite注册插件

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,7 +1,9 @@
 import { createServer as createViteDevServer } from "vite";
+import { createIndexHtmlPlugin } from "./plugin-island/indexHtml";
 
 export async function createDevServer(root = process.cwd()) {
   return createViteDevServer({
     root,
+    plugins: [createIndexHtmlPlugin()],
   });
 }
