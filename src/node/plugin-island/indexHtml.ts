228e6b6bad6e4c6b40a9fae9e133539b81e42c8d
Dannyzn <694518723@qq.com>
2023-03-10 23:01:18 +0800

    feat: vite 支持手动添加标签

--- src/node/plugin-island/indexHtml.ts
+++ src/node/plugin-island/indexHtml.ts
@@ -1,11 +1,32 @@
 import { readFile } from "fs/promises";
 import { Plugin } from "vite";
-import { DEFAULT_HTML_PATH } from "../constants";
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from "../constants";
 
 export function createIndexHtmlPlugin(): Plugin {
     return {
         name: "vite-ssg:index-html",
         apply: "serve",
+        // 插入入口 script 标签
+        transformIndexHtml(html, ctx) {
+            // const { path } = ctx;
+            // const { base } = ctx.server.config;
+            // const script = `<script type="module" src="${base}${path}"></script>`;
+            // return html.replace(/<\/body>/, script + "</body>");
+            return {
+                html,
+                tags: [ 
+                    {
+                        tag: 'script',
+                        attrs: {    
+                            type: 'module',
+                            // src: 'http://localhost:3000/src/main.tsx',
+                            src: `/@fs/${CLIENT_ENTRY_PATH}`,
+                        },
+                        injectTo: 'body',
+                    }
+                ],
+            }
+        },
         configureServer(server) {
             return () => {
                 server.middlewares.use(async (req, res, next) => {
